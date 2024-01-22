d6b1e47581303da594a422dcd6017f0e840a3bb9
hankangwen <13538898378@163.com>
2024-01-22 15:38:33 +0800

    [帧同步]定义服务端玩家类

---
+++ OnLineMobaGameServer/Player/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ProtoBuf;
+
+public class Player
+{
+    /// <summary>
+    /// 玩家GUID
+    /// </summary>
+    public uint guid;
+
+    /// <summary>
+    /// 所在房间号
+    /// </summary>
+    public int roomId = -1;
+
+    public Player(uint guid)
+    {
+        this.guid = guid;
+    }
+
+    /// <summary>
+    /// TCP发送
+    /// </summary>
+    /// <param name="msg"></param>
+    public void Send(IExtensible msg)
+    {
+        Console.WriteLine($"msg:{msg.ToString()}, guid:{guid}");
+        NetManager.Send(msg, guid);
+    }
+
+    /// <summary>
+    /// UDP发送
+    /// </summary>
+    /// <param name="msg"></param>
+    public void SendTo(IExtensible msg)
+    {
+        NetManager.SendTo(msg, guid);
+    }
+}
---
+++ OnLineMobaGameServer/Player/PlayerManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class PlayerManager
+{
+    /// <summary>
+    /// 所有玩家字典
+    /// </summary>
+    public static Dictionary<uint, Player> players = new Dictionary<uint, Player>();
+
+    /// <summary>
+    /// 获取玩家
+    /// </summary>
+    /// <param name="guid"></param>
+    /// <returns></returns>
+    public static Player GetPlayer(uint guid)
+    {
+        if (players.ContainsKey(guid))
+        {
+            return players[guid];
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 添加玩家
+    /// </summary>
+    /// <param name="guid"></param>
+    /// <param name="player"></param>
+    public static void AddPlayer(uint guid, Player player)
+    {
+        players.Add(guid, player);
+    }
+
+    /// <summary>
+    /// 移除玩家
+    /// </summary>
+    /// <param name="guid"></param>
+    public static void RemovePlayer(uint guid)
+    {
+        players.Remove(guid);
+    }
+}
