0497bd10b84e192fc80d2dfb1ffd5218becb394b
hankangwen <13538898378@163.com>
2024-01-23 16:43:36 +0800

    [帧同步]帧同步主循环

--- OnLineMobaGameServer/Room/RoomManager.cs
+++ OnLineMobaGameServer/Room/RoomManager.cs
@@ -4,6 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class RoomManager
+public static class RoomManager
 {
+    /// <summary>
+    /// 最大房间号
+    /// </summary>
+    private static int maxId = 0;
+
+    /// <summary>
+    /// 房间列表
+    /// </summary>
+    public static Dictionary<int, Room> rooms = new Dictionary<int, Room>();
+
+    /// <summary>
+    /// 获取房间
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public static Room GetRoom(int id)
+    {
+        if(rooms.TryGetValue(id, out var room))
+            return room;
+        return null;
+    }
+
+    /// <summary>
+    /// 添加房间
+    /// </summary>
+    /// <returns></returns>
+    public static Room AddRoom()
+    {
+        maxId++;
+        Room room = new Room();
+        room.id = maxId;
+        room.lockStepManager.room = room;
+        rooms.Add(room.id, room);
+        return room;
+    }
 }
